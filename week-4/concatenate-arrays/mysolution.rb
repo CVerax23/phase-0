b5946b6e279d34f71355f8bf684deb47e7d025da
CVerax23 <Cverax23@gmail.com>
2015-12-07 11:23:45 -0600

    4.6.5 Final

---
+++ week-4/concatenate-arrays/mysolution.rb
@@ -0,0 +1,12 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+  solution = Array.new
+  solution = array_1 + array_2
+  solution = solution.flatten
+end
