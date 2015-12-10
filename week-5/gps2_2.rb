722b8fb0f8756581f8d8d88700fe69fcc16a6103
CVerax23 <Cverax23@gmail.com>
2015-12-09 22:50:20 -0600

    rel 2 unfinished

---
+++ week-5/gps2_2.rb
@@ -0,0 +1,37 @@
+# PSEUDOCODE
+
+
+# Method to create a list
+# input: Hash keys & values
+  # [fill in any steps here]
+  # Set hash keys 
+  # print 
+# output: return full hash 
+
+def g_list(hash)
+	return hash
+end
+	p list = g_list({ "lemonade" => 2 , "tomatoes" => 3})
+
+def add_item(list, new_item, qty)
+		list[new_item] = qty
+	end
+add_item(list, "onions", 1)
+p list 
+# steps:
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
