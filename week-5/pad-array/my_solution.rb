36be90ab2f9ece2f938f6cc36518b806b6a41743
CVerax23 <CVerax23@gmail.com>
2015-12-13 23:52:31 -0600

    Inital commit

---
+++ week-5/pad-array/my_solution.rb
@@ -0,0 +1,49 @@
+Pad an Array
+
+# I worked on this challenge [by myself, with: ]
+
+# I spent [] hours on this challenge.
+
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+
+
+# 0. Pseudocode
+
+# What is the input?
+# What is the output? (i.e. What should the code return?)
+# What are the steps needed to solve the problem?
+
+
+# 1. Initial Solution
+
+class Array
+  def pad!(min_size, value = nil)
+    x = min_size - self.count #take the argument and subtract the count from it.
+    x.times do
+     self << value
+    end# I want to add the value x number of times to the array
+    
+    self
+  end
+  
+  def pad(min_size, value = nil)
+  self.clone.pad!(min_size, value)
+  end
+end
+
+
+
+def pad(array, min_size, value = nil) #non-destructive
+  # Your code here
+end
+
+
+# 3. Refactored Solution
+
+
+
+# 4. Reflection
