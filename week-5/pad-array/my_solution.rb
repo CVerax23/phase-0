88e255dc709a737d829ed033d430218facc5d368
CVerax23 <CVerax23@gmail.com>
2015-12-14 01:22:26 -0600

    Submit

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -1,8 +1,8 @@
-Pad an Array
+#Pad an Array
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge [by myself, with: Susan S]
 
-# I spent [] hours on this challenge.
+# I spent [1.5] hours on this challenge.
 
 
 # Complete each step below according to the challenge directions and
@@ -13,37 +13,63 @@ Pad an Array
 
 # 0. Pseudocode
 
-# What is the input?
+# What is the input? 
+# Array object that takes a non-negative integer min array size
+# Optional value to pad defaulting to nil
+
 # What is the output? (i.e. What should the code return?)
+# Array object 
+# passed parameters
 # What are the steps needed to solve the problem?
+# 				  Compare array to min_size
+# 				  If array is smaller calc difference
+# 				  Push the value
+# 				  Return the array
+#
 
 
 # 1. Initial Solution
-
 class Array
-  def pad!(min_size, value = nil)
-    x = min_size - self.count #take the argument and subtract the count from it.
-    x.times do
-     self << value
-    end# I want to add the value x number of times to the array
-    
-    self
+  def pad!(array, min_size, value = nil)
+    if self.length < min_size
+      (min_size - self.length).times do
+        self << value
+      end
+    end
+    return self
   end
-  
-  def pad(min_size, value = nil)
-  self.clone.pad!(min_size, value)
+
+  def pad(array, min_size, value = nil)
+    new_array = self.dup
+    if new_array.length < min_size
+      (min_size - new_array.length).times do
+        new_array << value
+      end
+    end
+    return new_array
   end
 end
 
 
-
-def pad(array, min_size, value = nil) #non-destructive
-  # Your code here
-end
 
 
 # 3. Refactored Solution
+class Array
+  def pad!(array, min_size, value = nil)
+    while self.length < min_size
+      self << value
+    end
+      return self
+  end
 
+  def pad(array, min_size, value = nil)
+    padded = self.dup
+    while padded.length < min_size
+      padded << value
+    end
+      return padded
+  end
+end
 
 
 # 4. Reflection
