ca40eb6251f8a5a2859294e11942a72876c3b354
CVerax23 <CVerax23@gmail.com>
2016-01-04 10:12:51 -0600

    7stuff

---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,97 @@
+ // JavaScript Olympics
+
+// I paired [with:Lindsey H] on this challenge.
+
+// This challenge took me [2] hours.
+
+
+// Warm Up
+
+/*	
+function win(athletes) {
+ for (i = 0; i < athletes.length; i++)
+ {
+ 	athletes[i].win = true;
+ 	console.log(athletes[i].name + " won the " + athletes[i].event)
+ }
+}
+
+person1 = {
+ name: 'John',
+ event: 'Basketball'
+
+}
+
+person2 = {
+ name: 'Jim',
+ event: 'Skiier'
+
+}
+
+person3 = {
+ name: 'Dan',
+ event: 'Luge'
+
+}
+var athletes = [person1, person2, person3]
+
+// win(athletes)
+
+// Jumble your words
+function reverse(text) {
+ var chars = text.split('');
+ chars.reverse();
+ console.log(chars.join(''));
+}
+
+str = "cat";
+reverse(str)
+
+
+// 2,4,6,8
+
+function evens(nums) {
+	var evennums = [];
+	for (i = 0; i < nums.length; i++){
+	if (nums[i] % 2 === 0)
+		evennums.push(nums[i]);
+	}
+console.log(evennums);
+return evennums;
+}
+
+evens([1,2,5,7,8])
+
+// "We built this city"
+
+
+var Athlete = function(name,age,sport,quote) {
+	this.name = name;
+	this.age = age;
+	this.sport = sport;
+	this.quote = quote;
+}
+
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+console.log(michaelPhelps.constructor === Athlete)
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+
+
+
+
+
+
+
+
+// Reflection
+
+1. What JavaScript knowledge did you solidify in this challenge?
+	
+	All of it lol.
+
+2. What are constructor functions?
+
+	
+3. How are constructors different from Ruby classes (in your research)?
+
+
