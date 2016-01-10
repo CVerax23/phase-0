bd54e120fa18d26eb1b69c4338b665aa0783f56f
CVerax23 <CVerax23@gmail.com>
2016-01-10 12:48:37 -0600

    8.4

---
+++ week-8/database-intro/my_solution.rb
@@ -0,0 +1,16 @@
+
+=begin 
+REFLECTION: 
+
+1. What are databases for?
+	Databases serve a special mission - to provide a well-organized mechanism for data manipulationby offering a quick and automated way for the information to be stored, managed, deleted or retrieved
+
+2. What is a one-to-many relationship?
+	In a one-to-many relationship, each row in the related to table can be related to many rows in the relating table
+
+
+3. What is a primary key? What is a foreign key? How can you determine which is which?
+	A primary key, also called a primary keyword, is a key in a relational database that is unique for each record. In context of relational databases, a foreign key is a field (or collection of fields) in one table that uniquely identifies a row of another table, one is the child, one is the parent..
+
+4. How can you select information out of a SQL database? What are some general guidelines for that?
+	Using the SELECT statement, the result is stored in a result table.
